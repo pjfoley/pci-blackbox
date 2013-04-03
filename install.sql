d07a6a6cd6b9b491ddf759cfbf0684b3991ea160
Joel Jacobson <joel@trustly.com>
2013-04-03 16:58:34 +0200

    Create plperlu functions as a superuser

--- install.sql
+++ install.sql
@@ -36,6 +36,11 @@ CREATE LANGUAGE plperlu;
 CREATE EXTENSION pgcrypto;
 CREATE EXTENSION "uuid-ossp";
 
+-- plperlu functions, can only be created by superusers
+\i pci/FUNCTIONS/card_to_json.sql
+\i pci/FUNCTIONS/card_from_json.sql
+\i pci/FUNCTIONS/http_post_xml.sql
+
 SET ROLE TO pci;
 
 \i pci/TABLES/cardnumberreferences.sql
@@ -44,15 +49,12 @@ SET ROLE TO pci;
 \i pci/FUNCTIONS/encrypt_card.sql
 \i pci/FUNCTIONS/encrypt_cvc.sql
 \i pci/FUNCTIONS/encrypt_card_cvc.sql
-\i pci/FUNCTIONS/card_to_json.sql
 \i pci/FUNCTIONS/authorise_payment_request.sql
 \i pci/FUNCTIONS/authorise_payment_request_3d.sql
 \i pci/FUNCTIONS/decrypt_card.sql
 \i pci/FUNCTIONS/decrypt_cvc.sql
-\i pci/FUNCTIONS/card_from_json.sql
 \i pci/FUNCTIONS/format_adyen_authorise_request.sql
 \i pci/FUNCTIONS/format_adyen_authorise_request_3d.sql
-\i pci/FUNCTIONS/http_post_xml.sql
 \i pci/FUNCTIONS/parse_adyen_authorise_response.sql
 \i pci/FUNCTIONS/parse_adyen_authorise_response_3d.sql
 
