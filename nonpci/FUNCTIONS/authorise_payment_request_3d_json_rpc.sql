eba793d387b805115552350d941a65b99458f195
Joel Jacobson <joel@trustly.com>
2013-04-11 17:13:47 +0200

    Add JSON-RPC wrapper for authorise_payment_request_3d() function

---
+++ nonpci/FUNCTIONS/authorise_payment_request_3d_json_rpc.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE FUNCTION Authorise_Payment_Request_3D_JSON_RPC(
+OUT PSPReference text,
+OUT ResultCode text,
+OUT AuthCode integer,
+OUT RefusalReason text,
+_PCIBlackBoxURL text,
+_PSP text,
+_MerchantAccount text,
+_URL text,
+_Username text,
+_Password text,
+_BrowserInfoAcceptHeader text,
+_BrowserInfoUserAgent text,
+_IssuerMD text,
+_IssuerPAResponse text,
+_ShopperIP inet
+) RETURNS RECORD AS $BODY$
+
+my ($PCIBlackBoxURL, $PSP, $MerchantAccount, $URL, $Username, $Password, $BrowserInfoAcceptHeader, $BrowserInfoUserAgent, $IssuerMD, $IssuerPAResponse, $ShopperIP) = @_;
+
+use JSON::RPC::Simple::Client;
+my $c = JSON::RPC::Simple::Client->new($PCIBlackBoxURL);
+
+if ($PCIBlackBoxURL =~ m!^https://localhost!i) {
+    # Test-environment or local call, skip check
+    $c->{ua}->ssl_opts(verify_hostname => 0);
+}
+
+my $r = $c->authorise_payment_request_3d({
+    psp                     => $PSP,
+    merchantaccount         => $MerchantAccount,
+    url                     => $URL,
+    username                => $Username,
+    password                => $Password,
+    browserinfoacceptheader => $BrowserInfoAcceptHeader,
+    browserinfouseragent    => $BrowserInfoUserAgent,
+    issuermd                => $IssuerMD,
+    issuerparesponse        => $IssuerPAResponse,
+    shopperip               => $ShopperIP
+});
+return $r;
+$BODY$ LANGUAGE plperlu VOLATILE SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION Authorise_Payment_Request_3D_JSON_RPC(_PCIBlackBoxURL text, _PSP text, _MerchantAccount text, _URL text, _Username text, _Password text, _BrowserInfoAcceptHeader text, _BrowserInfoUserAgent text, _IssuerMD text, _IssuerPAResponse text, _ShopperIP inet) FROM PUBLIC;
+GRANT  ALL ON FUNCTION Authorise_Payment_Request_3D_JSON_RPC(_PCIBlackBoxURL text, _PSP text, _MerchantAccount text, _URL text, _Username text, _Password text, _BrowserInfoAcceptHeader text, _BrowserInfoUserAgent text, _IssuerMD text, _IssuerPAResponse text, _ShopperIP inet) TO GROUP nonpci;
